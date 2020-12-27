52d832f2d3f85e883b06af94aa28cccaf888b001
Megan Byers <meby3441@colorado.edu>
2020-12-26 21:07:26 -0700

    build design section

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $("#haas-house-logo").css({"animation": "fade-out 0.5s ease-out both"})
         $("#haas-house").css({"animation": "fade-in 1.75s cubic-bezier(.39,.575,.565,1.000) both"})
 
-   }, 2000);
+   }, 5000);
    
 })
 
@@ -28,8 +28,10 @@ $('.film-toggle').click(function(){
         $('#film-toggle-link').css("display", "block");
         $('.movie-nav').css("display", "none");
         $('.tv-11').css("animation", "fade-in 0.5s cubic-bezier(.39,.575,.565,1.000) both");
+        $('#film-iframe').removeAttr("src");
+
     } else {
-        $('#the-body').css({"transform": "scale(5)", "transform-origin": "59% 36%"});
+        $('#the-body').css({"transform": "scale(5)", "transform-origin": "59% 45%"});
         $('#film-iframe').css("display", "block");
         $('#film-toggle-link').css("display", "none");
         $('.movie-nav').css("display", "block");
@@ -38,7 +40,7 @@ $('.film-toggle').click(function(){
 });
 
 //film screen navigation functionality 
-var filmLinks = ["https://player.vimeo.com/video/487712165?title=0&byline=0&portrait=0", "https://player.vimeo.com/video/151901736?title=0&byline=0&portrait=0", "https://player.vimeo.com/video/97186301?title=0&byline=0&portrait=0"];
+var filmLinks = ["https://player.vimeo.com/video/494815946", "https://player.vimeo.com/video/494873166"];
 var currentVid = 0;
 $('#film-iframe').attr("src", filmLinks[currentVid]); //set initial video 
 //next button
@@ -61,7 +63,7 @@ $('.photo-toggle-link').click(function(){
     console.log("clicked tv-5-link");
     clickPhoto++;
     if(clickPhoto% 2 === 0){
-        $('#the-body').css({"transform": "scale(1)", "transform-origin": "62.5% 31%"});
+        $('#the-body').css({"transform": "scale(1)", "transform-origin": "15% 65%"});
         $('#tv-5-link').css("display", "block");
         $('.photo-frame-wrapper').css("display", "none");
         $('#tv-5-nav').css("display", "none");
@@ -71,7 +73,7 @@ $('.photo-toggle-link').click(function(){
 
         $('.tv-5').css("animation", "fade-in 0.5s cubic-bezier(.39,.575,.565,1.000) both");
     } else {
-        $('#the-body').css({"transform": "scale(5)", "transform-origin": "17% 55%"});
+        $('#the-body').css({"transform": "scale(5)", "transform-origin": "15% 65%"});
         $('#tv-5-link').css("display", "none");
         $('.photo-frame-wrapper').css("display", "block");
         $('#tv-5-nav').css("display", "block");
@@ -80,7 +82,7 @@ $('.photo-toggle-link').click(function(){
     }
 });
 
-var peopleCollection = ["src/casey-portrait.jpeg", "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/kmkrsltibnrzrsubcgvd/will-smith", "https://s3.amazonaws.com/sfc-datebook-wordpress/wp-content/uploads/sites/2/2020/07/MER9673f0f77451bb620d8210bf2cfa4_bravo0706-1024x684.jpg"]
+var peopleCollection = ["src/people/one.jpg", "src/people/two.jpg", "src/people/three.jpg", "src/people/four.jpg", "src/people/five.jpg", "src/people/six.jpg", "src/people/seven.jpg",  ]
 var placesCollection = ["https://ewscripps.brightspotcdn.com/dims4/default/3a54f41/2147483647/strip/true/crop/2038x1146+5+216/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2F26%2F12%2Fa6f367014ff6bd297b8b2fbe3c6e%2Fandrew-plutt.jpg", "https://bloximages.newyork1.vip.townnews.com/outtherecolorado.com/content/tncms/assets/v3/editorial/d/e5/de52bca7-16bc-5b16-8c06-b05e593ff698/5eecdc1b7713c.image.jpg?crop=1254%2C705%2C0%2C65&resize=1254%2C705&order=crop%2Cresize", "https://s1.it.atcdn.net/wp-content/uploads/2019/11/Colorado-road-trip-hero-800x600.jpg", "https://www.visittheusa.com/sites/default/files/styles/hero_l_x2/public/images/exp-background/2019-11/de9cba61-3eb7-40e0-a4a0-3aa523961e41.jpeg?itok=3Yf7hyME"]
 var thingsCollection = ["https://contenthub-static.grammarly.com/blog/wp-content/uploads/2018/05/things-to-write-about-760x400.jpg", "https://www.howsweeteats.com/wp-content/uploads/2019/07/tt-1-76.jpg", "https://cambridgewords.files.wordpress.com/2019/04/obvious_300x200.jpg?w=640", "https://www.howsweeteats.com/wp-content/uploads/2019/10/IMG_8425.jpg", "https://www.howsweeteats.com/wp-content/uploads/2019/03/tt-1-59.jpg", "https://assets.londonist.com/uploads/2018/05/i875/rainbow.jpg"]
 
@@ -149,7 +151,7 @@ $('.open-guide').click(function(){
 
 $('#designs-link').click(function(){
     console.log('clicked design ')
-    $('#the-body').css({"transform": "scale(5)", "transform-origin": "56.5% 88%"});
+    $('#the-body').css({"transform": "scale(5)", "transform-origin": "56.5% 90%"});
     $('#designs-link').css("display", "none");
     $('.design-tv-guide').css("display", "block");
     $('.tv-3-nav').css("display", "block");
@@ -157,7 +159,7 @@ $('#designs-link').click(function(){
 })
 
 $('#design-power-off').click(function(){
-    $('#the-body').css({"transform": "scale(1)", "transform-origin": "56.5% 88%"});
+    $('#the-body').css({"transform": "scale(1)", "transform-origin": "56.5% 92%"});
     $('#designs-link').css("display", "block");
     $('.design-tv-guide').css("display", "none");
     $('.tv-3-nav').css("display", "none");
