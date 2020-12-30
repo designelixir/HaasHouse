696793606c06e9926bb5b7e7ee215dc837e1ed6d
Megan Byers <meby3441@colorado.edu>
2020-12-30 02:48:35 -0700

    miracle shit i figured out zooming

--- js/index.js
+++ js/index.js
@@ -10,24 +10,26 @@
 var clickFilm = 0; 
 $('.film-toggle').click(function(){
     clickFilm++;
+    var tv11 = document.getElementById('tv-11').getBoundingClientRect();
+
     if(clickFilm % 2 === 0){
 
-        $('#the-body').css({"transform": "scale(1)", "transform-origin": "55.5% 31%"});
+        $('#the-body').css({"transform": "scale(1)", "transform-origin-x": (tv11.right -100), "transform-origin-y": (tv11.top + 50)});
         $('#film-toggle-link').css("display", "block");
         $('#film-iframe').css("display", "none");
         $('#film-toggle-link').css("display", "block");
         $('.movie-nav').css("display", "none");
         $('.tv-11').css("animation", "fade-in 0.5s cubic-bezier(.39,.575,.565,1.000) both");
-        $('#film-iframe').removeAttr("src");
-        
+        // $('#film-iframe').removeAttr("src");
+        $('#tv-11-screen').css({'background-color': '#1e1e1e'})
+
         
 
     } else {
-        $('#the-body').css({"transform": "scale(5)", "transform-origin": "59% 45%"});
+        $('#the-body').css({"transform": "scale(4)", "transform-origin-x": (tv11.right -100), "transform-origin-y": (tv11.top + 50)});
         $('#film-iframe').css("display", "block");
         $('#film-toggle-link').css("display", "none");
         $('.movie-nav').css("display", "block");
-        $('#tv-11-screen').css({'background-color': '#1e1e1e'})
         $('.tv-11').css("animation", "fade-in 0.5s cubic-bezier(.39,.575,.565,1.000) both");
     }
 });
@@ -53,10 +55,11 @@ $('#movie-nav-prev').click(function(){
 
 var clickPhoto = 0;
 $('.photo-toggle-link').click(function(){
-    console.log("clicked tv-5-link");
     clickPhoto++;
+    var tv5 = document.getElementById('tv-5').getBoundingClientRect();
+
     if(clickPhoto% 2 === 0){
-        $('#the-body').css({"transform": "scale(1)", "transform-origin": "70% 65%"});
+        $('#the-body').css({"transform": "scale(1)", "transform-origin-x": (tv5.right - 50), "transform-origin-y": (tv5.bottom)});
         $('#tv-5-link').css("display", "block");
         $('.photo-frame-wrapper').css("display", "none");
         $('#tv-5-nav').css("display", "none");
@@ -65,7 +68,7 @@ $('.photo-toggle-link').click(function(){
 
         $('.tv-5').css("animation", "fade-in 0.5s cubic-bezier(.39,.575,.565,1.000) both");
     } else {
-        $('#the-body').css({"transform": "scale(5)", "transform-origin": "15% 60%"});
+        $('#the-body').css({"transform": "scale(4)", "transform-origin-x": (tv5.left), "transform-origin-y": (tv5.top + 75 )});
         $('#tv-5-link').css("display", "none");
         $('.photo-frame-wrapper').css("display", "block");
         $('#tv-5-nav').css("display", "block");
@@ -144,8 +147,9 @@ $('.open-guide').click(function(){
 // DESIGN TV ###########################
 
 $('#designs-link').click(function(){
-    console.log('clicked design ')
-    $('#the-body').css({"transform": "scale(5)", "transform-origin": "56.5% 92%"});
+    var tv3 = document.getElementById('tv-3-designs').getBoundingClientRect();
+
+    $('#the-body').css({"transform": "scale(4)", "transform-origin-x": (tv3.right - 75), "transform-origin-y": (tv3.bottom)});
     $('#designs-link').css("display", "none");
     $('.design-tv-guide').css("display", "block");
     $('.tv-3-nav').css("display", "block");
@@ -153,7 +157,7 @@ $('#designs-link').click(function(){
 })
 
 $('#design-power-off').click(function(){
-    $('#the-body').css({"transform": "scale(1)", "transform-origin": "56.5% 92%", "width": "100%"});
+    $('#the-body').css({"transform": "scale(1)"});
     $('#designs-link').css("display", "block");
     $('.design-tv-guide').css("display", "none");
     $('.tv-3-nav').css("display", "none");
@@ -177,38 +181,70 @@ $('#design-tv-guide-button').click(function(){
 
 
 var webCollection = ["src/casey-portrait.jpeg", "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/kmkrsltibnrzrsubcgvd/will-smith", "https://s3.amazonaws.com/sfc-datebook-wordpress/wp-content/uploads/sites/2/2020/07/MER9673f0f77451bb620d8210bf2cfa4_bravo0706-1024x684.jpg"]
+var webCollectionDescript = ["project one", "project two", "project three"]
+
 var printCollection = ["https://ewscripps.brightspotcdn.com/dims4/default/3a54f41/2147483647/strip/true/crop/2038x1146+5+216/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2F26%2F12%2Fa6f367014ff6bd297b8b2fbe3c6e%2Fandrew-plutt.jpg", "https://bloximages.newyork1.vip.townnews.com/outtherecolorado.com/content/tncms/assets/v3/editorial/d/e5/de52bca7-16bc-5b16-8c06-b05e593ff698/5eecdc1b7713c.image.jpg?crop=1254%2C705%2C0%2C65&resize=1254%2C705&order=crop%2Cresize", "https://s1.it.atcdn.net/wp-content/uploads/2019/11/Colorado-road-trip-hero-800x600.jpg", "https://www.visittheusa.com/sites/default/files/styles/hero_l_x2/public/images/exp-background/2019-11/de9cba61-3eb7-40e0-a4a0-3aa523961e41.jpeg?itok=3Yf7hyME"]
+var printCollectionDescript = ["project 1", "project 2", "project 3", "project 4"]
+
 var filmCollection = ["https://contenthub-static.grammarly.com/blog/wp-content/uploads/2018/05/things-to-write-about-760x400.jpg", "https://www.howsweeteats.com/wp-content/uploads/2019/07/tt-1-76.jpg", "https://cambridgewords.files.wordpress.com/2019/04/obvious_300x200.jpg?w=640", "https://www.howsweeteats.com/wp-content/uploads/2019/10/IMG_8425.jpg", "https://www.howsweeteats.com/wp-content/uploads/2019/03/tt-1-59.jpg", "https://assets.londonist.com/uploads/2018/05/i875/rainbow.jpg"]
+var filmCollectionDescript = ["project #1", "project #2", "project #3", "project #4", "project #5", "prokect # 6"]
+
+var fullDesignCollection = [].concat(webCollection, printCollection, filmCollection);
+var fullDesignDescript = [].concat(webCollectionDescript, printCollectionDescript, filmCollectionDescript)
+
 
 
 $('.channels').click(function(){
     $('.design-tv-guide').css("display", "none");
-    currentChannel = 0;
     var activeCategory = [];
+    var activeDescript = [];
 
-    if($(this).index() === 0){activeCategory = webCollection; }
-    else if($(this).index()===1){activeCategory = printCollection; }
-    else if($(this).index()===2){activeCategory = filmCollection;}
+    if ($(this).attr("id") == "design-guide-close"){activeCategory = fullDesignCollection; activeDescript = fullDesignDescript }
+    else if($(this).index() === 0){activeCategory = webCollection; activeDescript = webCollectionDescript }
+    else if($(this).index()===1){activeCategory = printCollection; activeDescript = printCollectionDescript}
+    else if($(this).index()===2){activeCategory = filmCollection; activeDescript = filmCollectionDescript}
 
+    var categoryChannelCount = activeCategory.length;
     
     $('#tv-3-bg-target').attr("src", activeCategory[0]);
-    
+    $('#design-description').html(activeDescript[0])
+    var current = 0;
 
+    //next button
     $('#design-nav-forward').click(function(){
-        if (currentChannel === activeCategory.length -1 ){currentChannel = -1;} //loops back 
-        currentChannel++;
-        $('#tv-3-bg-target').attr("src", activeCategory[currentChannel]);
-    
+        current++;
+        if ((current) >= categoryChannelCount ){  //loops back 
+            current = 0; 
+            $('#tv-3-bg-target').attr("src", activeCategory[current]);
+            $('#design-description').html(activeDescript[current]);
+        }
+        $('#tv-3-bg-target').attr("src", activeCategory[current]);
+        $('#design-description').html(activeDescript[current]);
+
+        console.log(current);
     });
     //back button
     $('#design-nav-backward').click(function(){
-        if(currentChannel <= 0){currentChannel = activeCategory.length;}
-        currentChannel--;
-        $('#tv-3-bg-target').attr("src", activeCategory[currentChannel]);
+        
+        console.log("TRIGGERRRRR" + current)
+        if (current <= 0){
+            
+            console.log('targeted')
+             
+            $('#tv-3-bg-target').attr("src", activeCategory[categoryChannelCount]);
+            $('#design-description').html(activeDescript[categoryChannelCount]);
+            current = categoryChannelCount;
+        } else {
+            $('#tv-3-bg-target').attr("src", activeCategory[current]);
+            $('#design-description').html(activeDescript[current]);
+        }
+        
+        
+        
     });
-    
+        
 
-})
+    })
 
 
 var aboutClickCount = 0;
